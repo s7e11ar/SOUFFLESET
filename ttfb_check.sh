63b7254643ba4ab1ac45a2f758798c45bddf09a7
s7e11ar <you@example.com>
2021-05-12 21:06:32 -0400

    Added logs directory and ttfb_check.sh

---
+++ ttfb_check.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Program name: ttfb_check.sh
+
+date
+cat /home/chive/SOUFFLESET/url_list.txt | while read output
+do
+	torsocks curl --silent -o /dev/null -w "Connect: %{time_connect} TTFB: %{time_starttransfer} Total time: %{time_total} Target: $output\n" $output
+done
