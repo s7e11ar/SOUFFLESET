7447b75078edd530d565478d8b63baa6fd0cdaf1
ste11ar <ste11ar@hedwig.localdomain>
2022-03-30 23:09:22 -0400

    Removed hardcoded path

--- ttfb_check.sh
+++ ttfb_check.sh
@@ -2,7 +2,7 @@
 # Program name: ttfb_check.sh
 
 date
-cat /home/chive/SOUFFLESET/url_list.txt | while read output
+cat $PWD/url_list.txt | while read output
 do
 	torsocks curl --silent -o /dev/null -w "Connect: %{time_connect} TTFB: %{time_starttransfer} Total time: %{time_total} Target: $output\n" $output
 done
